6845baf70cda8f3a6360ff9ead089a2d489222e0
Faisal Gazi <faisal.gazi@hackney.gov.uk>
2024-07-23 14:12:58 +0100

    Moved a file
    TestDataGenerator is now in TestHelpers ns

--- HousingFinanceInterimApi.Tests/V1/TestHelpers/TestDataGenerator.cs
+++ HousingFinanceInterimApi.Tests/V1/TestHelpers/TestDataGenerator.cs
@@ -5,18 +5,15 @@ namespace HousingFinanceInterimApi.Tests.V1.TestHelpers
 {
     public static class TestDataGenerator
     {
-        private static readonly Fixture _fixture = new();
-
         /// <summary>
-        /// Property Reference known locally as  prop_ref
+        /// Property Reference aka prop_ref
         /// </summary>
         /// <returns></returns>
-        public static string PropRef
+        public static string PropRef()
         {
-            get
-            {
-                return string.Concat("9", _fixture.Create<int>().ToString().PadLeft(7, '0').AsSpan(0, 7));
-            }
+            var fixture = new Fixture();
+            // 8 Digit random number that leads with 9 to prevent clashes e.g. "90023871"
+            return string.Concat("9", fixture.Create<int>().ToString().PadLeft(7, '0').AsSpan(0, 7));
         }
     }
 }
